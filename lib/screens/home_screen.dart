5dd7103ae8ab6e0863936ca30b25284cd671f664
shahdat-sajib <shahdat35-2757@diu.edu.bd>
2022-05-19 01:30:51 +0600

    home page bg + richtext added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,8 +8,37 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(),
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/main_page_bg.png"),
+            alignment: Alignment.topCenter,
+            fit: BoxFit.fitWidth,
+            ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: size.height * .1),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: RichText(text: TextSpan(
+                style: Theme.of(context).textTheme.displaySmall,
+                children: [
+                  TextSpan(text: "What you want to \nlearn"),
+                  TextSpan(text: " today ?",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),),
+                ]
+              )),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
