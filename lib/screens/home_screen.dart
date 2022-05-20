077259cfa8964b19a835a8ade9e517e3873c6503
shahdat-sajib <shahdat35-2757@diu.edu.bd>
2022-05-21 00:50:19 +0600

    homepage best deal part added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:online_library/consttants.dart';
-
-import '../widgets/book_rating.dart';
-import '../widgets/two_side_rounded_button.dart';
+import '../widgets/ReadingListCard.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -63,131 +58,74 @@ class HomeScreen extends StatelessWidget {
                 ],
               ),
             ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ReadingListCard extends StatelessWidget {
-  final String image;
-  final String title;
-  final String auth;
-  final double rating;
-  final void Function() pressDetails;
-  final void Function() pressRead;
-
-  const ReadingListCard({
-    Key? key, 
-    required this.image, 
-    required this.title, 
-    required this.auth, 
-    required this.rating, 
-    required this.pressDetails, 
-    required this.pressRead,
-
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(left: 24, bottom: 40),
-      height: 245,
-      width: 202,
-      child: Stack(
-        children: [
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              height: 221,
-              decoration: BoxDecoration(color: Colors.white,
-              borderRadius: BorderRadius.circular(29),
-              boxShadow: [
-                BoxShadow(
-                  offset: Offset(0, 10),
-                  blurRadius: 33,
-                  color: kShadowColor,
-                )
-              ],
-              ),
-            ),
-          ),
-          Image.asset(
-            image,
-            width: 150,
-          ),
-          Positioned(
-            top: 35,
-            right: 10,
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24),
             child: Column(
               children: [
-                IconButton(
-                  icon: Icon(
-                    Icons.favorite_border,
-                  ),
-                  onPressed: (){},
-                  ),
-                  BookRating(score: rating,),
-              ],
-            ),
-              
-          ),
-          Positioned(
-            top: 160,
-            child: Container(
-              height: 85,
-              width: 202,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 24),
-                    child: RichText(
-                      maxLines: 2,
-                      text: TextSpan(
-                      style: TextStyle(color: kBlackColor),
-                      children: [
-                        TextSpan(text: "$title\n",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                        ),
-                        TextSpan(
-                          text: auth,
-                        style: TextStyle(
-                          color: kLightBlackColor,
-                        )),
-                      ]
-                    )),
-                  ),
-                  Spacer(), // this is for auto space
-                  Row(
+                RichText(text: TextSpan(
+                  style: Theme.of(context).textTheme.displaySmall,
+                  children: [
+                    TextSpan(text: "Best of the"),
+                    TextSpan(
+                      text: " day",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    )
+                  ]
+                ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 20),
+                  width: double.infinity,
+                  height: 235,
+                  child: Stack(
                     children: [
-                      GestureDetector(
-                        onTap: pressDetails,
+                      Positioned(
+                        top: 50,
+                        left: 0,
+                        right: 0,
                         child: Container(
-                          width: 101,
-                          padding: EdgeInsets.symmetric(vertical: 10),
-                          alignment: Alignment.center,
-                          child: Text("Details"),
+                          padding: EdgeInsets.only(left: 24, top: 24, right: size.width * .35),
+                        height: 185,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Color(0xFFEAEAEA).withOpacity(.45),
+                          borderRadius: BorderRadius.circular(29),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Bangladesh - 7th March of 1971",
+                            style: TextStyle(
+                              fontSize: 9,
+                              color: kLightBlackColor,
+                            ),),
+                            SizedBox(height: 5,),
+                            Text("Declaration of independence\nBangladesh",
+                            style: Theme.of(context).textTheme.titleMedium,),
+                            SizedBox(
+                                  height: 5,
+                                ),
+                                Text("Sheikh Mujib",
+                            style: TextStyle(
+                              color: kLightBlackColor,
+                            ),),
+                            SizedBox(height: 5,),
+                          ],
                         ),
                       ),
-                      Expanded(
-                        child: TwoSideRoundedButton(
-                          text: "Read",
-                          press: pressRead,
-                        ),
-                      )
+                      ),
+                      Positioned(
+                        right: 0,
+                        top: 0,
+                        child: Image.asset("assets/images/book-3.png"),
+                      width: size.width * .37,)
                     ],
                   ),
-                ],
-              ),
-            ),
-          ),
-        ],
+                ),
+              ],
+            ),)
+          ],
+        ),
       ),
     );
   }
