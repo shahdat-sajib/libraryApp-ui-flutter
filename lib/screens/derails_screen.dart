0d5c26f320cc4d73614d3e3418cc4bc4a24e9aff
shahdat-sajib <shahdat35-2757@diu.edu.bd>
2022-05-21 10:44:02 +0600

    detail page title section added

--- lib/screens/derails_screen.dart
+++ lib/screens/derails_screen.dart
@@ -21,7 +21,34 @@ class DetailsScreen extends StatelessWidget {
                   decoration: BoxDecoration(
                     image: DecorationImage(image: AssetImage("assets/images/bg.png"),
                     fit: BoxFit.fitWidth,),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(50),
+                    ),
                   ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 24),
+                    child: Column(
+                      children: [
+                        SizedBox(height: size.height * .1,),
+                        Row(
+                          children: [
+                            Expanded(child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Book Name",
+                                style: Theme.of(context).textTheme.displaySmall,),
+                                Text("Bolder part",
+                                style: Theme.of(context).textTheme.displaySmall.copyWith(fontWeight: FontWeight.bold),)
+                              ],
+                            ),),
+                            Image.asset("assets/images/book-1.png",
+                            height: 200,)
+                          ],
+                        )
+                      ],
+                    ),
+                    ),
                 ),
               ],
             )
